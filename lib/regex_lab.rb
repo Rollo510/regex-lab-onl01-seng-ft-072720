a1492ddadbc54326ee1423261204dc8d33a990ba
Kevin Britt <kbritt510@gmail.com>
2020-08-19 15:20:36 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -32,20 +32,16 @@ end
 # end
 
 def valid_phone_number?(phone)
-  if phone.match?(/([0123456789])([0123456789])([0123456789])([0123456789])([0123456789])([0123456789])([0123456789])/)
+  if phone.match(/([0-9] ?){10}/) || phone.match(/(\([0-9]{3}\)([0-9]{3}-[0-9]{4})\b)/) || phone.match(/\b([0-9]{7})\b/)
     true
   else
-    return false
+    false
   end
 end
 
 
 
-  # if phone.match?(/[0-9]{7}/)
-  # !valid_numbers.match?(/[a-z]||[A-Z]/)
-  #   false
-
-
+  
 
 
 
